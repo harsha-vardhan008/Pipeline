3a72e552449343d84f5ff9fe6c243273bc480e5d
Harsha Vardhan <yempally.harsha@kasmodigital.com>
2025-07-01 18:47:30 +0530

    Update main.py

--- Unstructured/main.py
+++ Unstructured/main.py
@@ -1,12 +1,7 @@
 from extract import extract
 from transform import trans
-<<<<<<< HEAD
-import load as l
-
-=======
 from load_data import connect_to_sqlserver, load_to_sql
 from log import log_table
->>>>>>> 703d8bdf800d01b810c45363edc031b117b13b01
 
 
 def main():
@@ -17,21 +12,6 @@ def main():
     project_df, client_df, team_df, milestone_df, fact_df = trans(df)
 
     # Connect to SQL Server
-<<<<<<< HEAD
-    engine = l.connect_to_sqlserver()
-
-    # Load and log each table
-    l.load_to_sql(project_df, "dim_project", engine)
-    
-
-    l.load_to_sql(client_df, "dim_client", engine)
-
-    l.load_to_sql(team_df, "dim_team", engine)
-
-    l.load_to_sql(milestone_df, "dim_milestone", engine)
-
-    l.load_to_sql(fact_df, "fact_project", engine)
-=======
     engine = connect_to_sqlserver()
 
     # Load and log each table
@@ -49,7 +29,6 @@ def main():
 
     load_to_sql(fact_df, "fact_project", engine)
     log_table(source="transform", destination="fact_project", action="insert", transformation_type="project_aggregation", engine=engine)
->>>>>>> 703d8bdf800d01b810c45363edc031b117b13b01
 
     print(" successfull.")
 
