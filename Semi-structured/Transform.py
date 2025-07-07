0cf0335bc3c7ad648ffe003a8b3bdae2cb3847d4
Harsha Vardhan <yempally.harsha@kasmodigital.com>
2025-07-07 17:52:22 +0530

    Delete Semi-structured directory

--- Semi-structured/Transform.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-def trans(df1):
-    dff1=df1.explode("technologies")
-    dff1["_id"]=dff1["_id"].astype(str)
-    dff1['start_date']=pd.to_datetime(dff1["start_date"])
-    dff1['end_date']=pd.to_datetime(dff1["end_date"])
-    return dff1
-
