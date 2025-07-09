51f7fa791098e527a80707ebff65e541289a99c4
Harsha Vardhan <yempally.harsha@kasmodigital.com>
2025-07-09 10:35:06 +0530

    WIP: local changes before pulling

--- Dynamoo/Structured/transform.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-def trans(df):
-    df1=df
-    # .explode("technologies")
-    df1["_id"]=df1["_id"].astype(str)
-    df1['start_date']=pd.to_datetime(df1["start_date"])
-    df1['end_date']=pd.to_datetime(df1["end_date"])
-    return df1
